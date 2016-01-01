f173512d97382df5cab6dd41405bb73abf1a7a16
waizuman <tstacctnu34t@gmail.com>
2016-01-01 18:55:17 -0500

    Update 18-UNION-UNION-ALL-INTERSECT-EXCEPT.sql

--- 18-UNION-UNION-ALL-INTERSECT-EXCEPT.sql
+++ 18-UNION-UNION-ALL-INTERSECT-EXCEPT.sql
@@ -1,11 +1,5 @@
-SELECT starid
-FROM planets
+SELECT starid FROM planets
 INTERSECT
-SELECT 3*starid
-FROM planets
-INTERSECT
-SELECT starid
-FROM planets
+SELECT 3*starid FROM planets
 EXCEPT
-SELECT 2*starid
-FROM planets
+SELECT 2*starid FROM planets
